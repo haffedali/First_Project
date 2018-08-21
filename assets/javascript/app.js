36d44ec1713fa659d549f6a3a54c064bd01d8f37
Haffed <haffedmcnair@gmail.com>
2018-08-21 12:13:02 -0700

    Javascript funcitonality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,6 @@ var status;
 var statusdetail;
 var results;
 var cityId;
-//var coord;
-//var coords;
 var city;
 var spot;
 var spots;
@@ -17,6 +15,7 @@ var detail = false;
 var spotId;
 var detailId;
 var scroll = $("<div>")
+//var detailDiv = $("<div>")
 var apiKey = "AIzaSyAKaTtDMQ-NxRqw8L_sWUrF-Ep4k9IeZwU";
 
 
@@ -66,7 +65,20 @@ geocoder.geocode({'address': city}, function(results, status) {
     service.getDetails(request, function(place, status) {
       console.log(status);
       if (status == google.maps.places.PlacesServiceStatus.OK) {
+        $('#mainDisplay').empty();
         console.log(place);
+        //var detailImg = $("<img>");
+        //detailImg.attr("src", place.photos[0].html_attributions[0]);
+        var detailDiv = $("<div>");
+        detailDiv.attr("class", "more-details");
+        //detailDiv.append(detailImg);
+        //detailDiv.append(place.photos[0].html_attributions[0]);
+        detailDiv.append(place.name);
+        detailDiv.append(place.rating);
+        detailDiv.append(place.formatted_address);
+        detailDiv.append(place.formatted_phone_number);
+        detailDiv.append(place.reviews[0].text)
+        $("#mainDisplay").append(detailDiv);
       };
     });
   };
@@ -79,29 +91,47 @@ geocoder.geocode({'address': city}, function(results, status) {
 });
 }
 
+/*
 function callbackdetail(place, statusdetail) {
   if (statusdetail === google.maps.places.PlacesServiceStatus.OK) {
     console.log("works");
     console.log(place);
+    $('#test').empty();
+    //var detailImg = $("<img>");
+    //detailImg.attr("src", place.photos[0].html_attributions[0]);
+    //console.log(detailImg);
+    var detailDiv = $("#<div>");
+    detailDiv.append(detailImg);
+    detailDiv.append(place.name);
+    detailDiv.append(place.rating);
+    detailDiv.append(place.formatted_address);
+    detailDiv.append(place.formatted_phone_number);
+    $("#test").append(detailDiv);
   }
 }
-
+*/
 function callback(results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK){
     if (detail === false) {
-      $("#test").empty();
+      $("#mainDisplay").empty();
       for (var i=0; i<results.length; i++) {
+        console.log(results[0]);
         spotId = results[i].place_id;
         spots = $("<div>");
         spotsIcon = $("<img>");
         spotsIcon.attr("src", results[i].icon);
+        spotsIcon.attr("id", "pic");
         spotButton = $("<button>");
-        spotButton.attr("id", "spotbutton");
+        spotButton.attr("id", "details");
         spotButton.attr("value", spotId);
+        var spotName = $("<div>")
+        spotName.text(results[i].name);
+        spotName.attr('id', "name");
         spots.append(spotsIcon);
-        spots.append(results[i].name);
+        spots.append(spotName);
         spots.append(spotButton);
-        $("#test").append(spots);
+        spots.attr("class", "spots");
+        $("#mainDisplay").append(spots);
       console.log(results[i]);
       } 
     }
@@ -123,11 +153,13 @@ $("#details").on("click", function(event){
   initMap();
 });
 
+/*
 $("#submit2").on("click", function(event){
   event.preventDefault();
   spot = $("#text2").val();
   initMap();
 });
+*/
 
 $("#hotels").on("click", function(event){
     event.preventDefault();
@@ -145,12 +177,13 @@ $("#food").on("click", function(event){
 
 
 
-$(document).on('click', '#spotbutton', function(){ 
+$(document).on('click', '#details', function(){ 
   // Your Code
   event.preventDefault();
   //console.log(this.value);
   detail = true;
   detailId = this.value;
+  //$("test").append(detailDiv);
   initMap();
   //detail = false;
 });
